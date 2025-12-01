0f396a9c515b1e278da69c701ee1f2b77bab1ff0
derickgb <penafloridaderickgabriel@gmail.com>
2025-12-01 15:40:04 +0800

    feat/create-table-doctors

--- veterinary.sql
+++ veterinary.sql
@@ -29,3 +29,12 @@ CREATE TABLE appointments;(
     FOREIGN KEY (animalid)
     REFERENCES animals(animalid)
 );
+
+CREATE TABLE doctors ( 
+doctorid INT PRIMARY KEY, 
+dfirstname VARCHAR(50), 
+dlastname VARCHAR(50), 
+speciality VARCHAR(50), 
+phone VARCHAR(15), 
+email VARCHAR(100) 
+); 
