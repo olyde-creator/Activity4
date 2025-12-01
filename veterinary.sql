aea765f204e075943e13085ace9f8928fbb53406
derickgb <penafloridaderickgabriel@gmail.com>
2025-12-01 15:51:33 +0800

    feat/create-table-medicalrecords

--- veterinary.sql
+++ veterinary.sql
@@ -47,3 +47,15 @@ CREATE TABLE invoices (
     FOREIGN KEY (appointid) 
     REFERENCES appointments (appointid)
 );
+
+CREATE TABLE medicalrecords ( 
+recordid INT PRIMARY KEY, 
+animalid INT, 
+recorddate DATETIME, 
+doctorid INT, 
+diagnosis VARCHAR(255), 
+prescription VARCHAR(255), 
+notes TEXT, 
+FOREIGN KEY (animalid) REFERENCES animals(animalid), 
+FOREIGN KEY (doctorid) REFERENCES doctors(doctorid) 
+); 
